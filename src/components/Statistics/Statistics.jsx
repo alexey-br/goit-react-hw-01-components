7ac7dc2a9fa97ccae5f815892fea183a30cae61d
Alexey Brazhnik <alexey.brazhnyk@gmail.com>
2022-08-27 18:01:49 +0300

    сделаны компоненты

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+
+export default function Statistics({ title, stats }) {
+  return (
+    <section className="statistics">
+      {title && <h2 className="title">{title}</h2>}
+
+      <ul className="stat-list">
+        {stats.map(({ id, label, percentage }) => (
+          <li className="item" key={id}>
+            <span className="label">{label}</span>
+            <span className="percentage">{percentage}%</span>
+          </li>
+        ))}
+
+        <li className="item">
+          <span className="label">.docx</span>
+          <span className="percentage">4%</span>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    })
+  ),
+};
